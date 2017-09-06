39e37837333caee87d05c939ce5b4f603f94645e
Fernando Felicissimo <ffelicissimo@gmail.com>
2017-09-06 18:16:21 +0100

    Up files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-techtest
+You have 24 hours to deploy at least 2 instance of the Python application.  
+You can:  
+* Choose your favorite OS, provider and virtualization.
+* Choose whatever tool or programming language you like to automate the process.
+
+## PYTHON APP
+The Python app is a simple API with two endpoints:  
+* **/set**: accept a POST call to increase the value of the Redis key 'total'
+* **/get**: accept a GET call to return the number of times you called the /set endpoint
+
+You can set these environment variables:  
+* ENV: set production or development environment. When in production mode, the app is only listening on localhost and debug mode is disabled  
+* REDIS_HOST: Redis hostname  
---
+++ python/api.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+
+import os
+import sys
+import redis
+
+from flask import Flask
+from flask import jsonify
+
+
+app = Flask(__name__)
+app.redis = redis.StrictRedis(host=os.getenv('REDIS_HOST', 'localhost'), socket_timeout=5)
+
+@app.route('/set', methods=['POST'])
+def set_value():
+    try:
+        app.redis.incr('total')
+    except redis.ConnectionError as e:
+        return jsonify({"status": "redis connection error"})
+    else:
+        return jsonify({"status": "ok"})
+
+@app.route('/get')
+def get_value():
+    try:
+        data = app.redis.get('total')
+    except redis.ConnectionError as e:
+        return jsonify({"status": "redis connection error"})
+    else:
+        return jsonify({"status": "ok", "data": data})
+
+
+if __name__ == "__main__":
+    env = os.getenv('ENV', 'production')
+    if env == 'production':
+        app.run()
+    elif env == 'development':
+        app.run(host='0.0.0.0', debug=True)
+    else:
+        print("ERROR: env {} not supported".format(env))
+        sys.exit(1)
